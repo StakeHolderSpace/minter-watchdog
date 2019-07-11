f98516e3aed2670ae6cf9c7d6eb474c131e0b181
Evgeniy <Evgeniy.Taburyanskiy@gmail.com>
2019-07-11 19:57:02 +0300

    обработка ошибок

--- assets/utils.js
+++ assets/utils.js
@@ -1,14 +1,22 @@
-export function handleError (error) {
-
-  if (error.code && !error.response) {
-    return console.error({
-      error : error.code,
-      url   : error.config.url,
-      params: error.config.params
-
+export const handleError = function (err) {
+  if (err.hasOwnProperty('response') && err.response) {
+    if (!err.response.hasOwnProperty('status')) {
+      console.log({ 'Network error': err.message })
+    } else {
+      if (err.response.data.error.hasOwnProperty('tx_result')) {
+        console.log({ err: err.response.data.error.tx_result.log })
+      } else {
+        console.log({ err: err.response.data.error.message })
+      }
+    }
+  } else if (err.hasOwnProperty('config') && err.config) {
+    console.log({
+      code  : err.code,
+      config: err.config
     })
-  } else if (error.response && error.data) {
-    return console.error(error.data)
+  } else if (err.hasOwnProperty('error') && err.error) {
+    console.log({ err: err.error })
+  } else {
+    console.log({ err: err })
   }
-
 }
